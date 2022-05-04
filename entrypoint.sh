369299412090cd4488613afb2a7b300b4d0d8e22
Ben Green <ben@bcgdesign.com>
2022-05-04 19:07:19 +0100

    Simplifying inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,29 +3,24 @@
 set -euo pipefail
 
 [[ -z "${1-}" ]] && \
-  echo "::error::You must pass a repository." && \
-  exit 1
-
-[[ -z "${2-}" ]] && \
-  echo "::error::You must pass a release name." && \
-  exit 1
-
-[[ -z "${3-}" ]] && \
-  echo "::error::You must pass a tag name." && \
-  exit 1
-
-[[ -z "${4-}" ]] && \
   echo "::error::You must pass a personal access token with release permissions." && \
   exit 1
 
-REPO="${1}"
-RELEASE_NAME="${2}"
-TAG_NAME="${3}"
-TOKEN="${4}"
+[[ -z "${2-}" ]] && \
+  echo "::error::You must pass the repository." && \
+  exit 1
+
+[[ -z "${3-}" ]] && \
+  echo "::error::You must pass the Pull Request title." && \
+  exit 1
+
+TOKEN="${1}"
+REPO="${2}"
+PR_TITLE="${3}"
 
 curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${TOKEN}" \
   "https://api.github.com/repos/${REPO}/releases" \
-  -d "{\"tag_name\":\"${TAG_NAME}\",\"target_commitish\":\"main\",\"name\":\"${RELEASE_NAME}\"}"
+  -d "{\"tag_name\":\"${PR_TITLE}\",\"target_commitish\":\"main\",\"name\":\"${PR_TITLE}\"}"
