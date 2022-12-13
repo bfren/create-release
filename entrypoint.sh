f059f6f911a7627cebca87bd6de432c9d259ed8c
bfren <ben@bcgdesign.com>
2022-12-13 09:13:59 +0000

    Adding prerelease support

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,11 +22,13 @@ TOKEN="${1}"
 REPO="${2}"
 BRANCH="${3}"
 TITLE="${4}"
-LATEST="${5:-true}"
+PRERELEASE="${5:-false}"
+LATEST="${6:-true}"
 
 curl \
   -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
   -H "Authorization: token ${TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/${REPO}/releases" \
-  -d "{\"tag_name\":\"${TITLE}\",\"target_commitish\":\"${BRANCH}\",\"name\":\"${TITLE}\",\"make_latest\":\"${LATEST}\"}"
+  -d "{\"tag_name\":\"${TITLE}\",\"target_commitish\":\"${BRANCH}\",\"name\":\"${TITLE}\",\"prerelease\":${PRERELEASE},\"make_latest\":\"${LATEST}\"}"
